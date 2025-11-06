5d45a67f42db49230893434ea623439f1e4cb8e0
Eduardo Ferreira <108958873+eduferreirac@users.noreply.github.com>
2025-11-05 22:29:40 -0300

    Update color-mode-menu.tsx

--- src/components/layout/sidebar/color-mode-menu.tsx
+++ src/components/layout/sidebar/color-mode-menu.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ClientOnly, Button, HStack, Text, Portal, IconButtonProps, Menu, Icon } from '@chakra-ui/react'
+import { ClientOnly, Button, HStack, Text, IconButtonProps, Menu, Icon } from '@chakra-ui/react'
 import { useTheme } from 'next-themes'
 import { useColorMode } from '@/components/ui/color-mode'
 import { BiCheck, BiDesktop, BiMoon, BiSun } from 'react-icons/bi'
@@ -36,35 +36,33 @@ export function ColorModeMenuButton(props: Props) {
           </Button>
         </Menu.Trigger>
 
-        <Portal>
-          <Menu.Positioner>
-            <Menu.Content minW="44">
-              <Menu.Item value="light" onClick={() => setColorMode('light')}>
-                <HStack w="full">
-                  <BiSun />
-                  <Text flex="1">Light</Text>
-                  {isActive('light') && <BiCheck />}
-                </HStack>
-              </Menu.Item>
+        <Menu.Positioner>
+          <Menu.Content minW="44">
+            <Menu.Item value="light" onClick={() => setColorMode('light')}>
+              <HStack w="full">
+                <BiSun />
+                <Text flex="1">Light</Text>
+                {isActive('light') && <BiCheck />}
+              </HStack>
+            </Menu.Item>
 
-              <Menu.Item value="dark" onClick={() => setColorMode('dark')}>
-                <HStack w="full">
-                  <BiMoon />
-                  <Text flex="1">Dark</Text>
-                  {isActive('dark') && <BiCheck />}
-                </HStack>
-              </Menu.Item>
+            <Menu.Item value="dark" onClick={() => setColorMode('dark')}>
+              <HStack w="full">
+                <BiMoon />
+                <Text flex="1">Dark</Text>
+                {isActive('dark') && <BiCheck />}
+              </HStack>
+            </Menu.Item>
 
-              <Menu.Item value="system" onClick={() => setColorMode('system')}>
-                <HStack w="full">
-                  <BiDesktop />
-                  <Text flex="1">System</Text>
-                  {isActive('system') && <BiCheck />}
-                </HStack>
-              </Menu.Item>
-            </Menu.Content>
-          </Menu.Positioner>
-        </Portal>
+            <Menu.Item value="system" onClick={() => setColorMode('system')}>
+              <HStack w="full">
+                <BiDesktop />
+                <Text flex="1">System</Text>
+                {isActive('system') && <BiCheck />}
+              </HStack>
+            </Menu.Item>
+          </Menu.Content>
+        </Menu.Positioner>
       </Menu.Root>
     </ClientOnly>
   )
