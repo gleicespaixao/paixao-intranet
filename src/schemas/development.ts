cc2e1b55ef5a7cd05b47b2c0ffc149dc1a94dc48
Eduardo Ferreira <108958873+eduferreirac@users.noreply.github.com>
2025-11-22 13:44:35 -0300

    Feat: realEstateDeveloper input

--- src/schemas/development.ts
+++ src/schemas/development.ts
@@ -2,10 +2,11 @@ import { z } from 'zod'
 
 export const schemaDevelopment = z.object({
   status: z.string(),
-  name: z.string().min(1, 'Informe o nome'),
+  name: z.string().min(2, 'Informe o nome'),
   neighborhood: z.object({
     value: z.string(),
     label: z.string().min(1, 'Informe o bairro do projeto')
-  })
+  }),
+  realEstateDeveloper: z.string().min(2, 'Informe a Incorporadora')
 })
 export type DevelopmentForm = z.infer<typeof schemaDevelopment>
