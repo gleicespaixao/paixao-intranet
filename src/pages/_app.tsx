d17f9c5520d3122d29ed4dfc727e2057bc96cef7
Eduardo Ferreira <108958873+eduferreirac@users.noreply.github.com>
2025-12-14 19:23:50 -0300

    Feat: bar-of-progress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,9 @@ import { Provider } from '@/components/ui/provider'
 import { SessionProvider, useSession } from 'next-auth/react'
 import { NextPageWithLayout } from '@/@types/next-page-with-layout'
 import { setApiAccessToken } from '@/lib/token-store'
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
+import { useRouter } from 'next/router'
+import ProgressBar from '@badrap/bar-of-progress'
 
 type AppPropsWithLayout = AppProps & { Component: NextPageWithLayout }
 
@@ -20,6 +22,23 @@ function SyncApiToken() {
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
+  const progress = useMemo(
+    () =>
+      new ProgressBar({
+        size: 2,
+        className: 'badrap-progress-bar',
+        delay: 100,
+        color: 'var(--chakra-colors-teal-500)'
+      }),
+    []
+  )
+  const router = useRouter()
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', progress.start)
+    router.events.on('routeChangeComplete', progress.finish)
+    router.events.on('routeChangeError', progress.finish)
+  }, [progress.finish, progress.start, router])
   return (
     <SessionProvider session={pageProps?.session}>
       <SyncApiToken />
